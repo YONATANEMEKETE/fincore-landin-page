5c518b73db56ec0fc4b694a2ed0a49a8224f9236
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-07-23 09:37:14 -0700

    data content

---
+++ src/Data/Data.ts
@@ -0,0 +1,186 @@
+const navLinks = [
+  {
+    name: 'Home',
+    ref: '#home',
+  },
+  {
+    name: 'About',
+    ref: '#about',
+  },
+  {
+    name: 'Features',
+    ref: '#features',
+  },
+  {
+    name: 'Pricing',
+    ref: '#pricing',
+  },
+];
+
+// TODO: infinite scroll images
+
+const whatWeDo = [
+  {
+    title: 'Automated Expenses',
+    description:
+      'Effortlessly streamline your finances with our automated expense tracking feature.',
+    id: 1,
+    // TODO: icons
+  },
+  {
+    title: 'Spending Overview',
+    description:
+      'Instantly track and manage expenses for smarter financial decisions.',
+    id: 2,
+  },
+  {
+    title: 'Track & Pay Invoices',
+    description:
+      'Streamline your workflow by tracking and paying invoices in one convenient platform',
+    id: 3,
+  },
+];
+
+const featuresNav = [
+  {
+    name: 'Hands - on support',
+    id: 1,
+  },
+  {
+    name: 'Premium features',
+    id: 2,
+  },
+  {
+    name: 'Legal & Security',
+    id: 3,
+  },
+];
+
+//  TODO: features content if possible
+
+const helpCenter = [
+  {
+    title: 'Account, Login & Billing',
+    desc: 'Troubleshooting guidance related to account, login issues, and billing concerns',
+    id: 1,
+    threads: 60,
+    images: [],
+    // TODO: icons
+  },
+  {
+    title: 'Privacy & Policies',
+    desc: 'We address common queries and concerns regarding our privacy policies.',
+    id: 2,
+    threads: 30,
+    images: [],
+  },
+  {
+    title: 'API Key Management',
+    desc: 'Guidance on generating, securing, and managing API keys for secure access.',
+    id: 3,
+    threads: 40,
+    images: [],
+  },
+  {
+    title: 'Error Code Deciphering',
+    desc: 'Understand and troubleshoot error codes encountered during integration.',
+    id: 4,
+    threads: 6,
+    images: [],
+  },
+  {
+    title: 'System Requirements',
+    desc: 'Information on system requirements and compatibility for optimal usage.',
+    id: 5,
+    threads: 30,
+    images: [],
+  },
+  {
+    title: 'System Downtime',
+    desc: 'Notifications and explanations regarding unexpected system downtimes.',
+    id: 6,
+    threads: 10,
+    images: [],
+  },
+];
+
+const testimonials = [
+  {
+    id: 1,
+    name: 'Annete Black',
+    position: 'Distribution Center Diesel Mechanic',
+    image: '',
+    text: 'So Yes, The Alcohol (Ethanol) In Hand Sanitizers Can Be Absorbed Through The Skin, But No, It Would',
+  },
+  {
+    id: 2,
+    name: 'Albert Flores',
+    position: 'Distribution Center Maintainance Technician',
+    image: '',
+    text: 'Even Factoring Differences In Body Weight Between Children And Adults Into Account, It Would Still N',
+  },
+  {
+    id: 3,
+    name: 'Theresa Webb',
+    position: 'Auto Parts Delivery Driver(part time)',
+    image: '',
+    text: 'Twenty 30-Second Applications Within Half An Hour Is Well In Excess Of Almost Anyones Use Of A Sani',
+  },
+];
+
+const footerLinks = [
+  {
+    title: 'Quick Link',
+    Links: [
+      'Home',
+      'How it Works',
+      'Features',
+      'Our Products',
+      'New Arrivals',
+      'Our Project',
+      'Blog',
+    ],
+  },
+  {
+    title: 'Who We Are',
+    Links: ['About Us', 'Contact US', 'Career', 'FAQ', 'Insight', 'Help'],
+  },
+  {
+    title: 'Follow Us',
+    Links: ['Facebook', 'Twitter', 'Instagram', 'Behance', 'Dribble'],
+  },
+  {
+    title: 'Company',
+    LInks: ['Blog', 'Integration', 'Support', 'Contact'],
+  },
+];
+
+const footerSocials = [
+  {
+    id: 'facebook',
+    logo: '',
+    // TODO: icons
+  },
+  {
+    id: 'instagram',
+    logo: '',
+  },
+  {
+    id: 'twitter',
+    logo: '',
+  },
+  {
+    id: 'LinkidIn',
+    logo: '',
+  },
+];
+
+export {
+  navLinks,
+  whatWeDo,
+  featuresNav,
+  helpCenter,
+  testimonials,
+  footerLinks,
+  footerSocials,
+};
