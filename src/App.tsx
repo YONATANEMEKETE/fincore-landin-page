1d0040e5fd95816f698c349e963435a39457642a
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-07-24 10:09:24 -0700

    images CDN

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { cn } from './lib/utils';
-
 function App() {
+  const imageArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <>
-      <h1 className={cn('text-5xl text-bg font-main font-bold')}>Hello</h1>
+      <div className="w-screen h-screen grid grid-cols-5 gap-6 justify-center items-center">
+        {imageArr.map((_, index) => (
+          <img
+            key={index}
+            src={`https://ik.imagekit.io/6qizpphtd1/HeroFin.png`}
+            height={500}
+            width={500}
+          />
+        ))}
+      </div>
     </>
   );
 }
